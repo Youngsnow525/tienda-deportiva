138bf07ead1de4ddc5bfe0c91e77fff57db711a0
dillerng <luisfernandochuc04@gmail.com>
2025-11-05 23:44:08 -0500

    version 1.1.1 mi primer travajo react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const productos = [
+    { id: 1, nombre: 'Pelota de fútbol', precio: 'S/ 60', imagen: "/img/pelota.jpg" },
+    { id: 2, nombre: 'Zapatillas deportivas', precio: 'S/ 120', imagen: "/img/zapatilla.jpg" },
+    { id: 3, nombre: 'Guantes de boxeo', precio: 'S/ 90', imagen: "/img/guantes-box.jpg" },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
+
+
+      <header>
+        <h2>esta pagina esta echa con react😊</h2>
+        <h1>Tienda Deportiva Espinar</h1>
+        <p>Equipamiento para campeones locales</p>
       </header>
+
+
+
+      <main>
+        <h2>Productos</h2>
+
+
+
+
+        <div className="productos">
+          {productos.map((item) => (
+            <div key={item.id} className="producto">
+              <img src={item.imagen} alt={item.nombre} />
+              <h3>{item.nombre}</h3>
+              <p>{item.precio}</p>
+              <button>Agregar al carrito</button>
+            </div>
+          ))}
+        </div>
+
+
+        <div>
+
+        </div>
+
+
+
+
+      </main>
+
+
+      <footer>
+        <p>&copy; 2025 Tienda Deportiva Espinar</p>
+      </footer>
     </div>
   );
 }
